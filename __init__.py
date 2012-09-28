5f233955cde2e16e8e1747faf60b57f97c27d99a
Jim Fulton <jim@zope.com>
2012-09-28 14:46:19 -0400

    First cut at base module to reduce tedium

---
+++ __init__.py
@@ -0,0 +1,32 @@
+import zim.monitor.interfaces
+import zope.interface
+
+class BaseMonitor:
+
+    zope.interface.implements(zim.monitor.interfaces.IMonitor)
+
+    def __init__(self):
+        self.__dict__.update(
+            dict((name, self.schema[name].default) for name in self.schema)
+            )
+
+    def expose_config(self):
+        return [self.schema[name] for name in sorted(self.schema)]
+
+    def setup(self, configs):
+        missing = sorted(
+            name for name in self.schema
+            if (self.schema[name].required and
+                name not in configs and
+                getattr(self, name) is None))
+        if missing:
+            raise TypeError("Required field %r ommitted." % missing)
+        self.__dict__.update(
+            dict(
+                (name, self.schema[name].fromUnicode(configs[name]))
+                for name in self.schema
+                if name in configs
+                ))
+
+    def get_config(self):
+        return dict((name, getattr(self, name)) for name in self.schema)
